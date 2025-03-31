89272bfebfc70fefc74797f7e186b1260d0d6a00
ryan-dot-g <115864216+ryan-dot-g@users.noreply.github.com>
2025-03-31 18:17:24 +1000

    Continuing updates
    Has functioning time loop, but it doesnt actually do anything yet

--- numerics.jl
+++ numerics.jl
@@ -1,6 +1,6 @@
 ### TODO 
-# Fix color mappings
 # convert operators to sparse?
+# fix color mappings so they r fixed despite changing phimin, phimax, and add colorbar 
 
 using LinearAlgebra, DifferentialEquations, JuMP, Ipopt,
         Plots, LaTeXStrings; 
@@ -24,14 +24,25 @@ f(ϵ) = κ * ϵ; # strain-dependent morphogen expression function
 ############ -------------------------------------------------- ############
 ############ ------------- NUMERICAL PARAMETERS --------------- ############
 ############ -------------------------------------------------- ############
-Ndisc = 100; # number of discretisation points on s 
+Ndisc = 1000; # number of discretisation points on s 
 smin = -pi/2; smax = pi/2; # bounds of s values
+dt = 0.1; # time discretisation
+tmax = 1; # max time 
+
+
+############ -------------------------------------------------- ############
+############ ----------- VISUALISATION PARAMETERS ------------- ############
+############ -------------------------------------------------- ############
+plotRes = 5; # how many timesteps per plot
+cmap = cgrad(:viridis); # colormap for morphogen concentration 
 
 ############ -------------------------------------------------- ############
 ############ ---------------- NUMERICAL SETUP ----------------- ############
 ############ -------------------------------------------------- ############
 Si = range(smin, smax, Ndisc); # grid of s values 
 ds = Si[2] - Si[1]; 
+Tn = 0:dt:tmax; # grid of t values 
+Ntimes = length(Tn); 
 
 ############ -------------------------------------------------- ############
 ############ ------------- DISCRETISED OPERATORS -------------- ############
@@ -52,18 +63,31 @@ Q[1,:] /= ds; Q[Ndisc, :] /= ds; # add 1/ds factor to f/b diffs on top/bottom ro
 Q /= ds^2; # scale 
 
 ############ -------------------------------------------------- ############
-############ ------------------ FUNCTIONALS ------------------- ############
+############ ------------- FUNCTIONALS AND QTYs --------------- ############
 ############ -------------------------------------------------- ############
+function trStrSq(X, Y, Xdash, Ydash)
+    # takes vectors of the surface parameterisation x(s), y(s) and derivatives w.r.t. s
+    # returns a vector containing the trace of the square of the strain tensor at each s
+    t1 = (P*Xdash).^2 .+ (P*Ydash).^2 .- R^2;
+    t2 = t1*0; 
+    t2[2:end-1] = X[2:end-1].^2 ./ (CosS[2:end-1].^2); # interior is just x/cos^2 
+    t2[1] = r^2; t2[end] = r^2; # boundary is when x(s)~r cos(s)
+    t2 = t2 .- R^2; 
+
+    en = 1/(4*R^4) * (t1.^2 .+ t2.^2);
+    return en; 
+end
 
 
-
 ############ -------------------------------------------------- ############
-############ ------- OTHER AUXILIARY / USEFUL FUNCTIONS ------- ############
+############ ---------- AUXILIARY / USEFUL FUNCTIONS ---------- ############
 ############ -------------------------------------------------- ############
-ϕ0 = 1/ζ * f(0.5 * ((r^2 - R^2)/R^2)^2); # steady-state morphogen scalar
-Phi0 = Si .* 0 .+ ϕ0; # steady-state morphogen over the full grid 
+ϕ0sc = 1/ζ * f(0.5 * ((r^2 - R^2)/R^2)^2); # steady-state morphogen scalar
+ϕ0 = Si .* 0 .+ ϕ0sc; # steady-state morphogen over the full grid 
+trStrSq0 = 0.5 * ((r^2 - R^2)/R^2)^2; # initial (constant) trace of strain tensor squared
 
 X0 = r * cos.(Si); Y0 = r * sin.(Si); # initial condition shape 
+X0dash = -Y0; Y0dash = X0; # initial derivatives 
 Xundef = R * cos.(Si); Yundef = R * sin.(Si); # undeformed shape
 
 # 2nd-order approximations of r cos(s), r sin(s) to use as initial guesses for 
@@ -74,21 +98,42 @@ y0quad(s) = r * (s);
 ############ -------------------------------------------------- ############
 ############ ------------- PRESAVING CALCULATIONS ------------- ############
 ############ -------------------------------------------------- ############
-CosS = cos.(Si); Sins = sin.(Si); # presaving trigs 
-TanS = 0 * CosS; TanS[2:end-1] = tan(Si[2:end-1]); # presaving Tan, with 0 on bdy to avoid undefineds
+CosS = cos.(Si); SinS = sin.(Si); # presaving trigs 
+TanS = 0 * CosS; TanS[2:end-1] = tan.(Si[2:end-1]); # presaving Tan, with 0 on bdy to avoid undefineds
 
+############ -------------------------------------------------- ############
+############ ------------------ UPDATE STEPS ------------------ ############
+############ -------------------------------------------------- ############
+function UpdateShape!(ϕ, X, Y, Xdash, Ydash)
+    # takes the current state data (shape and morphogen concentration), and updates X, Y 
+    # and derivatives IN PLACE, by minimising the energy functional 
+
+    # finally, update derivatives 
+    Xdash = P*X; Ydash = P*Y;
+end;
+
+function UpdateMorphogen!(ϕ, X, Y, ϵ)
+    # takes the current state data, and updates the morphogen concentration 
+    # IN PLACE, by performing an implicit update step 
+    ϕ = ϕ * 0 .+ ϕ;
+end;
+
+function ApplyBCs!(ϕ, X, Y)
+    # takes the current state, and enforces boundary conditions 
+    # Dirichelet BCs in X, and Neumann in ϕ and Y 
+    X = X*0 .+ X; 
+end;
 
 ############ -------------------------------------------------- ############
 ############ ------------ VISUALISATION FUNCTIONS ------------- ############
 ############ -------------------------------------------------- ############
-function visualise(X, Y, Phi, titleTxt = false)
+function visualise(ϕ, X, Y, titleTxt = false)
     # takes a surface discretised parameterisation and morphogen concentration
     # displays a plot of the deformed hydra, colored by the morphogen concentration 
     # optionally adds a provided title 
     # returns the plot object for displaying and saving
 
-    cmap = cgrad(:viridis); # produce colormap 
-    nphi = (Phi .- minimum(Phi)) ./ (maximum(Phi) - minimum(Phi)); # normalise for color 
+    nphi = (ϕ .- minimum(ϕ)) ./ (maximum(ϕ) - minimum(ϕ)); # normalise for color 
 
     # first, plot actual deformed shape 
     plt = plot(X, Y, lw = 2, color = :black, label = "Hydra shape");
@@ -106,18 +151,75 @@ function visualise(X, Y, Phi, titleTxt = false)
 
     # other plot necessities
     xlabel!("x"); ylabel!("y");
-    if titleTxt
+    lm = 2 * r; xlims!(-lm, lm); ylims!(-lm, lm);
+    if titleTxt isa String
         title!(titleTxt)
     end
 
-    return plt 
+    return plt;
 end
 
-plt = visualise(x0quad.(Si), y0quad.(Si), Phi0 .* Si);
+plt = visualise(ϕ0 .* Si, x0quad.(Si), y0quad.(Si));
 display(plt)
 
 
+############ -------------------------------------------------- ############
+############ --------------- CHECKING FORMULAS ---------------- ############
+############ -------------------------------------------------- ############
+checkFormulas = false
+if checkFormulas 
+    X = r * CosS; Y = r * SinS; 
+    Xdash = P*X; Ydash = P*Y; 
+
+    # check trace of strain tensor calc (CHECKED)
+    en = trStrSq(X, Y, Xdash, Ydash); 
+    # println(maximum( abs.(en .- trStrSq0) )); 
+
+    # check first-derivative matrix (CHECKED)
+    # error on bdy is about double error on interior, but still scales with ds as expected
+    XdashTrue = - r * SinS;
+    # println(XdashTrue .- Xdash);
+    # println(maximum( abs.(XdashTrue .- Xdash) ));
+
+    # check second-derivative matrix (CHECKED)
+    # error on bdy is much (10^6) higher than error on interior but does scale well 
+    Xddash = Q*X;
+    XddashTrue = -X;
+    println(XddashTrue .- Xddash);
+    println(maximum( abs.(XddashTrue .- Xddash) ));
+end
 
 
 
+############ -------------------------------------------------- ############
+############ ---------------- TIME EVOLUTION ------------------ ############
+############ -------------------------------------------------- ############
+X = X0; Y = Y0; 
+Xdash = X0dash; Ydash = Y0dash;
+ϕ = ϕ0 .* Si; 
+runsim = false 
+if runsim
+    for n = 1:Ntimes 
+        t = Tn[n];
+
+        # Step 1: update shape of X(s), Y(s) and derivatives 
+        UpdateShape!(ϕ, X, Y, Xdash, Ydash); 
+
+        # Step 2: compute new strain tensor squared 
+        ϵ = trStrSq(X, Y, Xdash, Ydash); 
+
+        # Step 3: evolve morphogen
+        UpdateMorphogen!(ϕ, X, Y, ϵ); 
+
+        # Step 4: apply BCs 
+        ApplyBCs!(ϕ, X, Y); 
+
+        # Step 5: visualise if necessary 
+        if mod(n, plotRes) == 0
+            tstr = round(t); 
+            pltAnim = visualise(ϕ, X, Y, "t = $tstr");
+            display(pltAnim);
+        end
+    end
+end
 
